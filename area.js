2d06f458e3a7d2475bb076a1fcfc0f0b188bb654
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2022-11-03 21:02:53 +0530

    Rectangle, triangle, circle
    area of rectangle, triangle, circle

--- area.js
+++ area.js
@@ -1,22 +1,22 @@
 //Area of rectangle.
 function areaRectangle(length, breadth){
-    area = length * breadth;
+    return length * breadth;
 }
 const answer = areaRectangle(5, 7);
-console.log("Area of Rectangle is = "+ area);
+console.log("Area of Rectangle is = "+ answer);
 
 
 // Area of traiangle.
 function areaTriangle(base, height){
-    area = (base * height) / 2;
+    return (base * height) / 2;
 }
 const answer1 = areaTriangle(6, 9);
-console.log("Area of Triangle is  = " + area);
+console.log("Area of Triangle is  = " + answer1);
 
 
 // Area of Circle
 function areaCircle(radius){
-    area = Math.PI * radius * radius;
+    return Math.PI * radius * radius;
 }
 const answer2 = areaCircle(8);
-console.log(area);
+console.log(answer2);
