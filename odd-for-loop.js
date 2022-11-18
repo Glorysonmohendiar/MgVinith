7562772bab2d5979dd05f7f2e81495d0c612c7cf
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2022-11-18 14:45:08 +0530

    Odd Number
    Odd number for loops

--- odd-for-loop.js
+++ odd-for-loop.js
@@ -1,6 +1,11 @@
-const array = [];
-
-for (i = 0; i < 10; i += 2) {
-  array.push(i);
+function odd(arr){
+  let array = [];
+  for (i = 0; i < arr.length; i ++) {
+    if(arr[i] % 2 != 0){
+      array.push(arr[i]);
+    }
+  }
+  return array;
 }
-console.log(array);
+const myArray = [1, 2, 3, 4, 5 ,6 ,7, 8, 9];
+console.log(odd(myArray));
