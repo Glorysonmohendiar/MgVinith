01f9f5d718e76b28d3c81a4b114cd5edb5d0fde5
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2022-11-24 16:06:44 +0530

    Return a Value from a Function with Return
    Return a Value from a Function with Return

--- callFun.js
+++
@@ -1,4 +0,0 @@
-function myFunction(param1, param2){
-    console.log(param1, param2);
-}
-myFunction("hello", "world");
---
+++ returnFun.js
@@ -0,0 +1,5 @@
+function myFun(num){
+return num + 5;
+}
+const myNum = 10;
+console.log(myFun(myNum));
