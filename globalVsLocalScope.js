b405ec32fe59c7b4fdc08178b0f4b8cd4ff082d8
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2022-11-24 16:15:41 +0530

    Global vs. Local Scope in Functions
    Global vs. Local Scope in Functions

---
+++ globalVsLocalScope.js
@@ -0,0 +1,8 @@
+
+const outerWear = "T-Shirt";
+function myOutfit() {
+  const outerWear = "sweater";
+  return outerWear;
+}
+
+console.log(myOutfit()); // The function myFun will return the string sweater because the local version of the variable is present.
