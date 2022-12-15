f82beef3574a811c9eb6879cfe5145b095339a2e
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2022-12-15 17:31:08 +0530

    Arrow Functions with Parameters
    Write Arrow Functions with Parameters

---
+++ arrowFun.js
@@ -0,0 +1,15 @@
+
+//Write Arrow Functions with Parameters
+
+const multiplier = (four, two) => four * two;
+
+console.log(multiplier(4, 2)); //console.log(multiplier(4, 2)); would return the value 8.
+
+console.log(multiplier(5, 7)); // console.log(multiplier(5, 7)); would return the value 35.
+
+
+// append contents of arr2 to arr1
+
+const myArray = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConca([1, 2], [3, 4, 5])); // console.log(myConca([1, 2], [3, 4, 5])); would return the array [1, 2, 3, 4, 5]
