c2a2144f636eab41f069302fad065eb6f3b14a2d
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2022-12-16 12:53:46 +0530

    Spread Operator to Evaluate Arrays In-Place.
    Spread Operator to Evaluate Arrays In-Place.

---
+++ spreadOperator.js
@@ -0,0 +1,8 @@
+
+// Use the Spread Operator to Evaluate Arrays In-Place.
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2 
+
+arr2 = [...arr1];
+console.log(arr2); // would return array ['JAN', 'FEB', "MAR', "APR', 'MAY']
