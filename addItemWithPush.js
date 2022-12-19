368591fced1cc735f77ba0e27ce9b6637041f5be
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2022-12-19 17:51:59 +0530

    Add Items to an Array with push() and unshift()
    Add Items to an Array with push() and unshift()

---
+++ addItemWithPush.js
@@ -0,0 +1,9 @@
+// Add Items to an Array with push() and unshift()
+
+function mixedNumbers(arr){
+    arr.push(7, 'VIII', 9);
+    arr.unshift('I', 2, 'Three');
+    return arr;
+}
+
+console.log(mixedNumbers([4, 'V', 'six']));
