463eec20e2c35ac446fb2d4861a527d36f71a750
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2022-12-20 19:59:09 +0530

    Copy Array Items Using slice()
    Copy Array Items Using slice()

---
+++ copyItmUsingSlice.js
@@ -0,0 +1,13 @@
+
+//Copy Array Items Using slice()
+
+
+function forecast(arr) {
+
+    return arr.slice(2, 4);
+  }
+  
+
+  console.log(
+    forecast(["cold", "rainy", "warm", "sunny", "cool", "thunderstorms"])
+  );
