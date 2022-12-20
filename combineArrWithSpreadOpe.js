8eb1d8b9267697b01a7957d8ebe762a1a74276a9
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2022-12-20 20:01:24 +0530

    Combine Arrays with the Spread Operator
    Combine Arrays with the Spread Operator

---
+++ combineArrWithSpreadOpe.js
@@ -0,0 +1,12 @@
+
+// Combine Arrays with the Spread Operator
+
+
+function spreadOut() {
+    let fragment = ["to", "code"];
+    let sentence = ["learning", ...fragment, "is", "fun"];
+    return sentence;
+  }
+  
+
+  console.log(spreadOut());
