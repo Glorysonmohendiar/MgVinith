5098665c36b030a9f04e9c4a68f1aed9450fdd82
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2022-12-20 20:03:01 +0530

    Check For The Presence of an Element With indexOf
    Check For The Presence of an Element With indexOf()

---
+++ presenceOfElementWithIndex.js
@@ -0,0 +1,7 @@
+
+// Check For The Presence of an Element With indexOf()
+
+function quickCheck(arr, elem) {
+    return arr.indexOf(elem) != -1;
+  }
+  console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
