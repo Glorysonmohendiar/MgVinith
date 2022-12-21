7b0c6da88de32b3c93c48872e0243b66fc6e7748
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2022-12-21 15:44:03 +0530

    Create complex multi-dimensional arrays
    Create complex multi-dimensional arrays

---
+++ complexMultidimensionalarr.js
@@ -0,0 +1,12 @@
+ 
+ // Create complex multi-dimensional arrays
+
+ let myNestedArray = [
+
+    'level 1',                   /* myNestedArray[0]             */
+    ['level 2'],	               /* myNestedArray[1][0]          */
+    [['level 3','deep']],	       /* myNestedArray[2][0][0]       */
+    [[['level 4','deeper']]],    /* myNestedArray[3][0][0][0]    */
+    [[[['level 5','deepest']]]]  /* myNestedArray[4][0][0][0][0] */
+
+  ];
