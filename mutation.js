9506de8744cc5a358a2a8d63d7019d12216e2f4d
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2023-01-03 16:20:40 +0530

    mutation
    mutation

---
+++ mutation.js
@@ -0,0 +1,11 @@
+
+// mutation
+function mutation(arr) {
+    const test = arr[1].toLowerCase();
+    const target = arr[0].toLowerCase();
+    for (let i = 0; i < test.length; i++) {
+      if (target.indexOf(test[i]) < 0) return false;
+    }
+    return true;
+  }
+  console.log(mutation('hello', 'hey'));
