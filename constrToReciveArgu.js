1bc0efa8ecec50e854a5e196e4e72e1a80a8a0c8
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2023-01-05 17:25:31 +0530

    Extend Constructors to Receive Arguments
    Extend Constructors to Receive Arguments

---
+++ constrToReciveArgu.js
@@ -0,0 +1,11 @@
+
+// Extend Constructors to Receive Arguments
+
+function Dog(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 4;
+  }
+  let terrier = new Dog();
+  terrier.name;
+  terrier.color;
