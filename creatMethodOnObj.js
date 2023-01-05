ba47432533305ffe336d1bcce5ebeeb4a753f3f6
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2023-01-05 17:21:29 +0530

    Create a Method on an Object
    Create a Method on an Object

---
+++ creatMethodOnObj.js
@@ -0,0 +1,12 @@
+
+// Create a Method on an Object
+
+let dog = {
+    name: "Spot",
+    numLegs: 4,
+    sayLegs: function(){
+      return "This dog has 4 legs."
+    }
+  };
+  
+  console.log(dog.sayLegs());
