3b65bd8fb9e29832371f89cd211bb93c1cb69884
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2023-01-09 17:12:38 +0530

    test of area of triangle
    test of area of triangle

---
+++ areaOfTrrai.test.js
@@ -0,0 +1,6 @@
+
+const area = require('./areaOfTrria');
+
+test('four multiply five devided by two is ten', () => {
+    expect((4 * 5) / 2).toBe(10);
+});
---
+++ areaOfTrria.js
@@ -0,0 +1,4 @@
+function areaOfTriangle(b, h){
+    return (b * h) / 2;
+}
+module.exports = areaOfTriangle;
