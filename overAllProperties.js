7cc797037533419943f725f44a2462c268f3e398
Glorysonmohendiar <104407042+Glorysonmohendiar@users.noreply.github.com>
2023-01-09 17:21:33 +0530

    Iterate Over All Properties
    Iterate Over All Properties

---
+++ overAllProperties.js
@@ -0,0 +1,22 @@
+function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype.numLegs = 4;
+  
+  let beagle = new Dog("Snoopy");
+  
+  let ownProps = [];
+  let prototypeProps = [];
+  
+
+  for (let property in beagle) {
+    if (Dog.hasOwnProperty(property)) {
+      ownProps.push(property);
+    } else {
+      prototypeProps.push(property);
+    }
+  }
+  console.log(beagle);
+  console.log(ownProps);
+  console.log(prototypeProps);
