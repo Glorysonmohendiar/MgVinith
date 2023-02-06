8b88465e4c90db03776a93a97accdd4643be26ea
Gloryson <104407042+Glorysonmohendiar@users.noreply.github.com>
2023-02-06 18:12:57 +0530

    testing empty function

---
+++ controller.test.js
@@ -0,0 +1,8 @@
+
+const controller = require("./controller");
+
+describe("cntroller.toCreat", ()=>{
+    it("controller should have toCraete", ()=>{
+        expect(typeof controller.toCreate).toBe("function");
+    })
+});
