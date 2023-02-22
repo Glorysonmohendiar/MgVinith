6e671685bc6f8b58c2dbc9c895ff69a3f4533648
Gloryson <104407042+Glorysonmohendiar@users.noreply.github.com>
2023-02-22 09:52:01 +0530

    testing the user details
    testing the user details

--- huntos.test.js
+++ huntos.test.js
@@ -1,21 +1,18 @@
 
 const getData = require('../huntos')
+const { testEnvironment } = require('../jest.config')
 
 
-describe('validate user obj', () =>{
-    it('this test should test the name of the user', async() =>{
-        //test('shold have "MGVinith',()=>{
-            let response = await getData()
-            for(let i = 0; i < response.length; i++){
-                let obj = response[i];
-            expect(obj.contact).toBe("9973770930")
-            expect(obj.language).toBe(4)
-            
-            
-            }
-        })
-    })
-//})
+describe('validate user obj',() =>{
+    it('this test should test the details of the user', async() =>{});
+    test('shold have contact 9973770930', async()=>{
+        let response = await getData()
+        for(let i = 0; i < response.length; i++){
+            let obj = response[i];
+        expect(obj.contact).toBe("9973770930");
+        }
+    });
+});
 
        
 
